a709dc1bdde80dafde65a5d42e8a135f9fe833ec
Jose <pjose14.jp38@yahoo.com>
2020-05-26 22:01:33 -0500

    almost done

---
+++ data/seeds/01-cars.js
@@ -0,0 +1,13 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('cars')
+  .del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('cars').insert([
+        {VIN: 'jp123hj123467898j8h', make: 'Jeep', model: 'Wrangler', milage: 12},
+        {VIN: 'jp123hj123467898j9h', make: 'Tesla', model:'Model 3', milage: 2},
+        {VIN: 'jp123hj123467898j0h', make: 'BMW', model:'M850i xDrive Gran Coupe', milage: 35}
+      ]);
+    });
+};
